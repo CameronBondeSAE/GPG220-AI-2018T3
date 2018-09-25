c72c2a28b00d815782f58ba4cc95a42a39bf2197
Venus <inkyinked@gmail.com>
2018-09-25 12:57:03 +1000

    The raycast now appears in the scene once game is put on play; next step is to have it turn based off whether it hits anything or not.

--- Assets/Prefabs/Venus Liao/CharacterAI.cs
+++ Assets/Prefabs/Venus Liao/CharacterAI.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class CharacterAI : MonoBehaviour
 {
 	public float MoveSpeed;
+	private Vector3 ForwardForce;
 
 	// Use this for initialization
 	void Start ()
 	{
-		
+		ForwardForce.z = MoveSpeed;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		GetComponent<Rigidbody>().AddForce(0, 0, MoveSpeed);
+		Vector3 ProjectedLocation = transform.position + ForwardForce;
+
+		GetComponent<Rigidbody>().AddForce(ForwardForce);
+		Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), MoveSpeed);
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward)*10, Color.green);
 	}
 }
