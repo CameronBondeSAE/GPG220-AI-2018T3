7bc2c77fa63cdb5013930886132064927546da5a
Venus <inkyinked@gmail.com>
2018-09-25 12:24:36 +1000

    Added own folder in Prefabs and a test character; test character now moves forward.

---
+++ Assets/Prefabs/Venus Liao/CharacterAI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAI : MonoBehaviour
+{
+	public float MoveSpeed;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		GetComponent<Rigidbody>().AddForce(0, 0, MoveSpeed);
+	}
+}
