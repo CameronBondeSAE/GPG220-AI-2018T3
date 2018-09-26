884cda5ae7a04040e9e48949e49e0777426ddb12
Venus <inkyinked@gmail.com>
2018-09-26 15:13:28 +1000

    Current state.

--- Assets/Prefabs/Venus Liao/CharacterAI.cs
+++ Assets/Prefabs/Venus Liao/CharacterAI.cs
@@ -5,21 +5,36 @@ using UnityEngine;
 public class CharacterAI : MonoBehaviour
 {
 	public float MoveSpeed;
+    public float RotateSpeed;
+
 	private Vector3 ForwardForce;
+    private Vector3 RotateForce;
+    private RaycastHit hit;
 
 	// Use this for initialization
 	void Start ()
 	{
 		ForwardForce.z = MoveSpeed;
+        //ForwardForce.x = MoveSpeed;
+        RotateForce.y = RotateSpeed;
+        //RotateForce = transform.rotation.eulerAngles;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		Vector3 ProjectedLocation = transform.position + ForwardForce;
+		//Vector3 ProjectedLocation = transform.position + ForwardForce;
 
-		GetComponent<Rigidbody>().AddForce(ForwardForce);
-		Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), MoveSpeed);
-        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward)*10, Color.green);
+        //'Addforce' is a world-based force (think wind)
+		GetComponent<Rigidbody>().AddRelativeForce(transform.forward);
+        //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(RotateForce), Time.fixedDeltaTime);
+        if (Physics.Raycast(transform.position, transform.TransformDirection(transform.forward), out hit, MoveSpeed))
+        {
+            //Debug.Log("Hit");
+            //ForwardForce.x = MoveSpeed;
+            transform.Rotate(RotateForce);
+            //RotateForce.y -= 10;
+        }
+        Debug.DrawRay(transform.position, transform.TransformDirection(transform.forward)*5, Color.green);
 	}
 }
