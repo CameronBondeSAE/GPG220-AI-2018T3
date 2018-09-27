2d2ac2a07024516b0172150530813e0fd604563d
Venus <inkyinked@gmail.com>
2018-09-27 11:22:44 +1000

    Feelers now work decently well; they avoid collision.

---
+++ Assets/Prefabs/Venus Liao/Feeler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Feeler : MonoBehaviour
+{
+    public GameObject PlayerChara;
+    public float RaycastScaler;
+    public float TurnSpeed;
+
+    private float RaycastDistance = 1;
+    private RaycastHit hit;
+    private Vector3 TurnVector;
+
+    // Use this for initialization
+    void Start ()
+    {
+        TurnVector.y = TurnSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 forwardDirection = transform.forward;
+
+        if(Physics.Raycast(transform.position, forwardDirection, out hit, RaycastDistance * RaycastScaler))
+        {
+            PlayerChara.GetComponent<Rigidbody>().transform.Rotate(TurnVector);
+            Debug.Log("HIT");
+        }
+	}
+}
