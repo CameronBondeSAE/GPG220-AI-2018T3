d2c5fb0f00da7b37e42b42b56f5a776eb3271492
Venus <inkyinked@gmail.com>
2018-10-05 16:42:47 +1000

    *praying to god I didn't screw anything over* (fixed some huge merging stuff)

--- Assets/Prefabs/Venus Liao/OwnSpace.cs
+++ Assets/Prefabs/Venus Liao/OwnSpace.cs
@@ -4,12 +4,8 @@ using UnityEngine;
 
 public class OwnSpace : MonoBehaviour
 {
-<<<<<<< HEAD
     private List<CharacterBase> characters;
 
-=======
-    private List<Neighbours> Neighbour;
->>>>>>> 39b44af4c35db3275068a0efd0aad0b26d033609
     void Start()
     {
         List<CharacterBase> characters = new List<CharacterBase>();
@@ -23,28 +19,14 @@ public class OwnSpace : MonoBehaviour
 
     public void OnTriggerEnter(Collider other)
     {
-<<<<<<< HEAD
+
         if (other.tag == "Character")
         {
             characters.Add(other.GetComponent<CharacterBase>());
-            foreach(CharacterBase chara in characters)
+            foreach (CharacterBase chara in characters)
             {
                 print(chara.characterName);
             }
-=======
-        if (other.tag == Character)
-        {
-            string NearbyName;
-            NearbyName = other.GetComponent<CharacterBase>().characterName;
-            print(NearbyName);
-
-            /*Neighbour.Add(new Neighbours(NearbyName, 0));
-
-            foreach (Neighbours chara in Neighbour)
-            {
-                print(chara.NeighbourName);
-            }*/
->>>>>>> 39b44af4c35db3275068a0efd0aad0b26d033609
         }
     }
 }
