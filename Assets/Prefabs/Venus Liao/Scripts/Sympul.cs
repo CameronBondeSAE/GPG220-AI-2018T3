72e161c3764c87eca4f28d34944ebf5a1299864e
Venus <inkyinked@gmail.com>
2018-10-17 16:03:19 +1100

    Finished mockup behaviour tree for chara 2.

---
+++ Assets/Prefabs/Venus Liao/Scripts/Sympul.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sympul : MonoBehaviour
+{
+    public bool BackStab
+    {
+        get
+        {
+            return _backStab;
+        }
+        set
+        {
+            _backStab = value;
+        }
+    }
+    public bool _backStab;
+
+    public int SlashDamage
+    {
+        get
+        {
+            return _slashDamage;
+        }
+        set
+        {
+            _slashDamage = value;
+        }
+    }
+    public int _slashDamage;
+
+    public bool WithinRange
+    {
+        get
+        {
+            return _withinRange;
+        }
+        set
+        {
+            _withinRange = value;
+        }
+    }
+    public bool _withinRange;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
