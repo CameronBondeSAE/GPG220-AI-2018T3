4a878f643a21d366e81ba62c8646e127d1fa9b29
Venus <inkyinked@gmail.com>
2018-10-17 15:31:28 +1100

    Finished up mock Behaviour Tree for character 1.

---
+++ Assets/Prefabs/Venus Liao/Scripts/Rhark.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rhark : MonoBehaviour
+{
+    /*public int ActionInt
+    {
+        get
+        {
+            return _actionInt;
+        }
+        set
+        {
+            _actionInt = value;
+        }
+    }
+    public int _actionInt;*/
+
+    public bool TwoEnemiesNearby
+    {
+        get
+        {
+            return _twoEnemiesNearby;
+        }
+        set
+        {
+            _twoEnemiesNearby = value;
+        }
+    }
+    public bool _twoEnemiesNearby;
+
+    public bool NoEnemiesNearby
+    {
+        get
+        {
+            return _noEnemiesNearby;
+        }
+        set
+        {
+            _noEnemiesNearby = value;
+        }
+    }
+    public bool _noEnemiesNearby;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
